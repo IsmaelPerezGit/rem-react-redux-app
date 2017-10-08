046031d02703ce13d38556b45f11090d6a4a8974
Ismael Perez <ismaelperezemail@gmail.com>
2017-10-07 19:04:59 -0700

    added more form fields that interact with state

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,22 +1,69 @@
 import React from 'react'
 
 class SignupForm extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      timezone: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <h1>Join our community!</h1>
 
         <div className="form-group">
           <label className="control-label">UserName</label>
-          <input
-            type="text"
-            name="username"
-            className="form-control"
-            />
+          <input value={this.state.username}
+            onChange={this.onChange}
+            type="text" name="username"
+            className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input value={this.state.email}
+            onChange={this.onChange}
+            type="text" name="email"
+            className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input value={this.state.password}
+            onChange={this.onChange}
+            type="password" name="password"
+            className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Confirm Password</label>
+          <input value={this.state.passwordConfirmation}
+            onChange={this.onChange}
+            type="password" name="passwordConfirmation"
+            className="form-control"/>
         </div>
 
         <div>
-          <button className="btn btn-primary btn-lg">
+          <button className="btn btn-primary btn-md  col-md-12 ">
             Sign up
           </button>
         </div>
